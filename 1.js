0909f703633d87fe6c871487f405abf6a3f6de33
NhocHackNasa <trungngoc9455@gmail.com>
2021-07-08 22:03:49 +0700

    ok7

--- 1.js
+++ 1.js
@@ -1,4 +1,10 @@
-﻿ $(function(){
- 
-})  
+﻿$(document).ready(function () {
+    var vitrikhoiedu = $("#khoiedu").offset().top;
+    console.log(vitrikhoiedu);
+
+    $(".btn-xuong,.kinhnghiem").click(function (e) { 
+        $("body,html").animate({scrollTop:vitrikhoiedu},800)
+        
+    });
+});
  
