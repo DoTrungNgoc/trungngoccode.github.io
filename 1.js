5d5a7ebdd4a301a446ec0fee60119f86825cd172
NhocHackNasa <trungngoc9455@gmail.com>
2021-07-08 22:23:43 +0700

    ok8

--- 1.js
+++ 1.js
@@ -1,7 +1,5 @@
 ﻿$(document).ready(function () {
     var vitrikhoiedu = $("#khoiedu").offset().top;
-    console.log(vitrikhoiedu);
-
     $(".btn-xuong,.kinhnghiem").click(function (e) { 
         $("body,html").animate({scrollTop:vitrikhoiedu},800)
         
